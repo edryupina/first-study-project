0fbb425774a8f4d4accb1453b3e946503dcaf4e1
edryupina1 <edryupina@aplana.com>
2017-12-05 22:48:51 +0300

    окончательная версия калькулятора

---
+++ src/main/java/calculator.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class calculator {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.printf("Перед вами примитивный калькулятор. После каждого действия не забывайте нажимать кнопку ENTER \n");
+        System.out.printf("               введите ПЕРВОЕ число ");
+        float numb1 = in.nextFloat();
+        System.out.printf("введите ЗНАК операции (+,-,* или /) ");
+        String operator = in.next();
+        System.out.printf("               введите ВТОРОЕ число ");
+        float numb2 = in.nextFloat();
+        float summa = numb1 + numb2;
+        float razn = numb1 - numb2;
+        float umn = numb1 * numb2;
+        float del = numb1 / numb2;
+        if (operator.equals("+")) {
+            System.out.printf("сумма двух чисел =                  %.4f \n", summa);
+        }
+        if (operator.equals("-")) {
+            System.out.printf("разница двух чисел =                %.4f \n", razn);
+        }
+        if (operator.equals("*")) {
+            System.out.printf("умножение двух чисел =              %.4f \n", umn);
+        }
+        if (operator.equals("/")) {
+            System.out.printf("деление двух чисел =                %.4f \n", del);
+        }
+    }
+}
