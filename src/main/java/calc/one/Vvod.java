6673d0a495253de882a1cfe46ce71406fe829c92
edryupina1 <edryupina@aplana.com>
2018-01-13 18:30:30 +0300

    калькулятор "современный"

--- src/main/java/calc/one/Vvod.java
+++ src/main/java/calc/one/Vvod.java
@@ -1,13 +1,15 @@
 package calc.one;
+import java.util.Scanner;
 
 public class Vvod {
    public static void main(String args[]){
+      Scanner in = new Scanner(System.in);
       System.out.println("введите ПЕРВОЕ число");
-      double numb1 = Error.doubleNum();
+      double numb1 = in.nextDouble();
       System.out.println("введите ЗНАК операции");
-      char znak = Error.operation();
+      char znak = in.next().charAt(0);
       System.out.println("введите ВТОРОЕ число");
-      double numb2 = Error.doubleNum();
+      double numb2 = in.nextDouble();
       System.out.println("получилось ИТОГО");
       Class calc;
       switch (znak){
